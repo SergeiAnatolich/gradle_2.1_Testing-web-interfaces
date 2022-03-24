5a27b274749ad97f4a37f4a2055310a042dc1a7a
SergeiAnatolich <92755716+SergeiAnatolich@users.noreply.github.com>
2022-03-24 08:34:28 +0300

    add WebDriverManager and headless

--- src/test/java/ru/netology/OrderSeleniumTest.java
+++ src/test/java/ru/netology/OrderSeleniumTest.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class OrderSeleniumTest {
@@ -16,12 +18,16 @@ class OrderSeleniumTest {
 
     @BeforeAll
     public static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
     public void setUp() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
